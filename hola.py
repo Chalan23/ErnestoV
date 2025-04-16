1164e5022abe043645f67ab379c1bf873502cc71
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-04-16 16:58:02 -0400

    numero mayor

--- hola.py
+++ hola.py
@@ -80,7 +80,9 @@ n2=int(input())
 print("ingrese numero 3") 
 n3=int(input())
 
-if n1 >= n2 and n1 >= n3:
-print("El numero mayor es ", n1)
-elif n2>=n1 and n2>=n3
-print("")
+if n1>n2 and n1>n3:
+    print("El numero mayor es ", n1)
+elif n2>n1 and n2>n3:
+    print("El numero mayor es ", n2)
+else:
+    print("El numero mayor es ", n3)
