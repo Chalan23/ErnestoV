820f4c3915a906c8df922330e5d4ef255a5dbeb9
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-04-21 11:18:32 -0400

    votos

--- for.py
+++ for.py
@@ -45,24 +45,76 @@
 # print("su promedio es", prom)
 
 
+# # REVISAR
+# # Cantidad de alumnos y sus promedios 
 
-# Cantidad de alumnos y sus promedios 
+# cantA=int(input("ingrese la cantidad de alumnos"))
 
-cantA=int(input("ingrese la cantidad de alumnos"))
+# for j in range(cantA):
+#     print("ingrese nota del alumno ", j+1)
+#     cantN=int(input())
+#     suma=0
+#     for i in range(cantN):
+#         print("ingrese nota", i+1, "del alumno ", j+1,  "(Use valores decimales)")
+#         nota=float(input())
+#         suma += nota
+#         # suma=suma+nota
+#     prom=suma/cantN
+#     print("su promedio es", prom)
+#     if prom >4:
+#         print("El alumno aprobo")
+#     else:
+#         print("El alumno reprobo")
 
-for j in range(cantA):
-    print("ingrese nota del alumno ", j+1)
-    cantN=int(input())
-    suma=0
-    for i in range(cantN):
-        print("ingrese nota", i+1, "del alumno ", j+1,  "(Use valores decimales)")
-        nota=float
-        (input())
-        suma += nota
-        # suma=suma+nota
-    prom=suma/cantN
-    print("su promedio es", prom)
-    if prom >4:
-        print("El alumno aprobo")
+
+
+# # Pida al usuario un numero y sume todos los digitos del 1 hasta ese digito
+
+# num=int(input("Ingrese un numero: "))
+# suma=0
+# for i in range(num):
+#     suma+=i+1
+# print("La suma de los numeros es", suma)
+
+
+
+# # par e impar
+
+# num=int(input("Ingrese un numero: "))
+# if num % 2==0:
+#     print("El numero es par")
+
+# else:
+#     print("El numero es impar")
+
+
+
+# #pida al usuario una cantidad de numeros y verifique si es par o impar
+
+# cant=int(input("ingrese la cantidad de numeros"))
+# for i in range(cant):
+#     cant=0
+#     num=int(input("Ingrese un numero: "))
+#     if num % 2==0:
+#         print("El numero es par")
+
+#     else:
+#         print("El numero es impar")
+
+
+#designe dos votantes, pida cantidad de votantes y muestre los resultados
+#verifique quien gano y considere un empate.
+
+print("Cuantas personas votan")
+num=int(input())
+
+bueno=int(input("ingrese la cantidad de votos"))
+for i in range(bueno):
+    bueno=0
+
+    num=int(input("Ingrese un numero: "))
+    if num % 2==0:
+        print("Bueno")
+
     else:
-        print("El alumno reprobo")
+        print("malo")
