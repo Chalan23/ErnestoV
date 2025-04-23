1b9f12e7dd9f03374e3b46a78a99dd374afc1bc1
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-04-23 10:45:37 -0400

    tarea

---
+++ ejePractica.py
@@ -0,0 +1,63 @@
+# ejercicios vistos en pseint pasados a python para practicar
+# -------------------------------------------------------------------------------------------------
+# # 1.- ingrese la edad y calcule cuantos dias a vivido
+# edad = int(input("Cuantos años tienes? "))
+
+# diasVida = edad * 365
+
+# print ("Su edad en dias es ", diasVida)
+
+# -------------------------------------------------------------------------------------------------
+
+# # 2.- pedir un numero y evaluar si es negativo y positivo 
+# num = int(input("Ingresa un numero "))
+
+# if num > 0:
+#     print ("El numero es positivo")
+
+# elif num < 0:
+#     print("El numero es negativo") 
+
+# else:
+#     print("El numero es 0")
+
+# -------------------------------------------------------------------------------------------------
+
+# # 3.- pedir un numero y evaluar si es par o impar 
+# num = int(input("Ingrese un numero "))
+
+# if num % 2 == 0:
+#     print("Su numero es par")
+
+# else: 
+#    print("Su nuemro es impar")
+
+# -------------------------------------------------------------------------------------------------
+
+# # 4.- pedir ingresar 3 numeros y calcular el promedio
+
+# nota1 = float(input("Ingrese su nota 1: "))
+# nota2 = float(input("Ingrese su nota 2: "))
+# nota3 = float(input("Ingrese su nota 3: "))
+
+# promedio = (nota1 + nota2 + nota3) / 3
+
+# print ("Su promedio es: ", promedio)
+
+# -------------------------------------------------------------------------------------------------
+
+# # 5.- pedir el nombre y repetirlo 5 veces.
+# nombre = str(input("Cual es tu nombre? "))
+
+# for i in range(5):
+#     print("Hola", nombre, "que tengas un buen dia")
+
+# -------------------------------------------------------------------------------------------------
+
+# # 6.- tabla de multiplicar 
+# num=int(input("Ingrese un numero para su tabla de multiplicar: "))
+
+# for i in range(1,11): 
+#     print(num, "X" , i, "=" , i*num)
+
+# -------------------------------------------------------------------------------------------------
