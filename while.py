e353877ed565e0d32ef3b26159867665041b52c9
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-04-28 09:20:12 -0400

    act

--- while.py
+++ while.py
@@ -15,21 +15,48 @@
 #     print("Clave correcta! Binevenido")
 
 
-#clave 3 intentos maximos 
+# #clave 3 intentos maximos 
  
-clave = 123
-intentos = 0
-max_intentos = 3
+# clave = 123
+# intentos = 0
+# max_intentos = 3
 
-while intentos < max_intentos:
-    password = int(input("Introduce la clave: "))
-    if password == clave:
-        print("¡Clave correcta! Bienvenido")
-        break
-    else:
-        print("Clave incorrecta, vuelve a intentarlo")
-        intentos += 1
+# while intentos < max_intentos:
+#     password = int(input("Introduce la clave: "))
+#     if password == clave:
+#         print("¡Clave correcta! Bienvenido")
+#         break
+#     else:
+#         print("Clave incorrecta, vuelve a intentarlo")
+#         intentos += 1
 
-if intentos == max_intentos:
-    print("Has superado el máximo de intentos")
+# if intentos == max_intentos:
+#     print("Has superado el máximo de intentos")
 
+
+
+
+# #pida al usuario el limite inferior y superior de rango
+# # despues genere un numero al azardentro de un rango
+# # el segundo numero no debe ser menor que el primero 
+# # pero debe darle la oportunidad al usuario de ingresar otro 
+
+# import random
+
+# print("Ingrese un numero")
+# n1=int(input("Ingrese el primer numero: "))
+# n2=int(input("Ingrese el segundo numero: "))
+
+# numRan=random.randint(n1,n2)
+# print(numRan)
+
+# while n1 <= n2:
+#     print("El numero debe ser mayor al anterior")
+#     n2=int(input("ingrese otro numero mayor al anterior"))
+    
+# numRan=random.randint(n1,n2)
+
+# print(numRan)
+
+
+import random
