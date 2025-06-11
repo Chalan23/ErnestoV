12841f5789fad408887d6c3cacfe9d98267b3267
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-06-11 17:19:16 -0400

    diccionarios

--- modulo3/clase11-06.py
+++ modulo3/clase11-06.py
@@ -25,63 +25,67 @@
 # print("El usuario selecciono la nota", notas[n-1])
 
 # -------------------------------------------------------------
-# # Tarea
-# # Crear programa de manejo de notas
-# # 1.- Ingresar nota
-# # 2.- Borrar nota
-# # 3.- Mostar notas
-# # 4.- Sacar promedio, nota mayor y nota menor
-# # 5.- Limpiar todas las notas
-# # 6.- Salir
+# # # Tarea
+# # # Crear programa de manejo de notas
+# # # 1.- Ingresar nota
+# # # 2.- Borrar nota
+# # # 3.- Mostar notas
+# # # 4.- Sacar promedio, nota mayor y nota menor
+# # # 5.- Limpiar todas las notas
+# # # 6.- Salir
 
-notas = []
+# notas = []
 
-while True:
-    try:
-        print("""
-                1.- Ingresar nota
-                2.- Borrar nota
-                3.- Mostar notas
-                4.- Sacar promedio, nota mayor y nota menor
-                5.- Limpiar todas las notas
-                6.- Salir
-            """)
-        opcion = int(input("Ingresa una opcion: "))
+# while True:
+#     try:
+#         print("""
+#                 1.- Ingresar nota
+#                 2.- Borrar nota
+#                 3.- Mostar notas
+#                 4.- Sacar promedio, nota mayor y nota menor
+#                 5.- Limpiar todas las notas
+#                 6.- Salir
+#             """)
+#         opcion = int(input("Ingresa una opcion: "))
         
-    except Exception():
-        print("Debe ingresar un numero valido")
+#     except Exception():
+#         print("Debe ingresar un numero valido")
         
-    match opcion:
-        case 1:
-            nota = float(input("Ingresa una nota: "))
-            notas.append(nota)
-        case 2:
-            for num, n in enumerate(notas): # sirve para enumerar elemntos
-                print(num+1,".-",n) # sirve para enumerar elemntos
-            nota = float(input("Ingresa la nota que quieres borrar: "))
-            notas.remove(nota)
-            notas.pop
-        case 3:
-            print("Las notas que ingresaste son:")
-            for nota in notas:
-                print(nota)
-        case 4:
-            if notas:
-                promedio = sum(notas) / len(notas)
-                notaMayor = max(notas)
-                notaMenor = min(notas)
-                print(f"Promedio: {promedio}, Nota Mayor: {notaMayor}, Nota Menor: {notaMenor}")
-            else:      
-                print("No hay notas para calcular")
-        case 5:
-            notas.clear()
-            print("Todas las notas han sido eliminadas.")
-        case 6:
-            print("Saliendo del sistema")
-            break
-        case _:
-            print("Error, ingresa una opcion valida.")
+#     match opcion:
+#         case 1:
+#             nota = float(input("Ingresa una nota: "))
+#             notas.append(nota)
+#         case 2:
+#             for num, n in enumerate(notas): # sirve para enumerar elemntos
+#                 print(num+1,".-",n) # sirve para enumerar elemntos
+#             nota = float(input("Ingresa la nota que quieres borrar: "))
+#             notas.remove(nota)
+#             notas.pop
+#         case 3:
+#             print("Las notas que ingresaste son:")
+#             for nota in notas:
+#                 print(nota)
+#         case 4:
+#             if notas:
+#                 promedio = sum(notas) / len(notas)
+#                 notaMayor = max(notas)
+#                 notaMenor = min(notas)
+#                 print(f"Promedio: {promedio}, Nota Mayor: {notaMayor}, Nota Menor: {notaMenor}")
+#             else:      
+#                 print("No hay notas para calcular")
+#         case 5:
+#             notas.clear()
+#             print("Todas las notas han sido eliminadas.")
+#         case 6:
+#             print("Saliendo del sistema")
+#             break
+#         case _:
+#             print("Error, ingresa una opcion valida.")
 
 # -----------------------------------------------------------------
 
+# # listas complejas explicacion
+# listaCompleja = [[3,4],[8,9]]
 
+# print(listaCompleja[0][1])
+# print(listaCompleja[1][1])
---
+++ modulo3/diccProfe11-06.py
@@ -0,0 +1,35 @@
+dic = {"nombre":"Diego Robles",
+       "numero": 12345678,
+       "Casado": True
+       }
+
+dic["ciudad"]= "santiago"
+
+print(dic)
+
+# for key, value in dic.items():
+#     # print(key, value)
+#     print(dic["nombre"])
+
+# ------------------------------------------------------------------
+    
+# personas=[{"nombre":"jony bravo",
+#        "numero": 12345678,
+#        "Casado": True
+#        },
+#         {"nombre":"pedro pascual",
+#        "numero": 12345678,
+#        "Casado": False
+#        },
+#         {"nombre":"Diego Robles",
+#        "numero": 12345678,
+#        "Casado": True
+#        }
+# ]
+
+# print(personas[2]["numero"])
+
+# -------------------------------------------------------------------
+
+# texto = input("Ingrese un texto")
+# palabra = input("Ingrese la palabra que desea buscar")
