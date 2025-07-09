18c2927e51e2f774b76266148a45d11cab22c212
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-07-09 01:40:17 -0400

    1

--- modulo3/prueba4.py
+++ modulo3/prueba4.py
@@ -20,79 +20,83 @@
 # solicita al usuario seleccionar una opción para realizar las acciones descritas, 
 # manejando entradas inválidas y errores de forma controlada.
 
-cliente = {
-    1:{"nombre":"ernesto", 
-       "entrada":"V",
-       "codigo":"EErr23"},
-    2:{"nombre":"meme", 
-       "entrada":"G",
-       "codigo":"MMee23"}
-}
+# cliente = {
+#     1:{"nombre":"ernesto", 
+#        "entrada":"V",
+#        "codigo":"EErr23"},
+#     2:{"nombre":"meme", 
+#        "entrada":"G",
+#        "codigo":"MMee23"}
+# }
 
-def comprarEntrada(dicc):
-    nombre=input("Ingresa el nombre: ")
-    entrada=input("Ingresa la entrada general(G) o vip(V): ")
-    print("""
-          Recuerda el codigo debe tener 6 caracteres
-          2 mayuscula, 2 minuscula y 2 numero""")
-    codigo=input("Ingresa el codigo: ")
-    if codiValido(codigo):
-        clienteNuevo = max(dicc.keys()) + 1
-        dicc[clienteNuevo]={"nombre":nombre,
-                       "entrada":entrada,
-                       "codigo":codigo}
-    else:
-        print("El parametro del codigo no es correcto")
+# def comprarEntrada(dicc):
+#     nombre=input("Ingresa el nombre: ")
+#     entrada=input("Ingresa la entrada general(G) o vip(V): ")
+#     print("""
+#           Recuerda el codigo debe tener 6 caracteres
+#           2 mayuscula, 2 minuscula y 2 numero""")
+#     codigo=input("Ingresa el codigo: ")
+#     if codiValido(codigo):
+#         clienteNuevo = max(dicc.keys()) + 1
+#         dicc[clienteNuevo]={"nombre":nombre,
+#                        "entrada":entrada,
+#                        "codigo":codigo}
+#     else:
+#         print("El parametro del codigo no es correcto")
 
-def borrarCliente(dicc):
-    mostrarCliente(dicc)
-    borrar = int(input("Cual quieres borrar: "))
-    del dicc[borrar]
+# def borrarCliente(dicc):
+#     mostrarCliente(dicc)
+#     borrar = int(input("Cual quieres borrar: "))
+#     del dicc[borrar]
     
-def mostrarCliente(dicc):
-    for key, cliente in dicc.items():
-        print(key, cliente)
+# def mostrarCliente(dicc):
+#     for key, cliente in dicc.items():
+#         print(key, cliente)
 
-def codiValido(codigo):
-    mayuscula = False
-    minuscula = False
-    numero = False
-    for palabra in codigo:
-        if palabra.isupper():
-            mayuscula = True
-        if palabra.islower():
-            minuscula = True
-        if palabra.isdigit():
-            numero = True
-    if mayuscula and minuscula and numero and len(codigo)==6:
-        print("Los parametros estan bien")
-        return True
-    else:
-        print("Los parametros estan mal")
-        return False
+# def codiValido(codigo):
+#     mayuscula = False
+#     minuscula = False
+#     numero = False
+#     for palabra in codigo:
+#         if palabra.isupper():
+#             mayuscula = True
+#         if palabra.islower():
+#             minuscula = True
+#         if palabra.isdigit():
+#             numero = True
+#     if mayuscula and minuscula and numero and len(codigo)==6:
+#         print("Los parametros estan bien")
+#         return True
+#     else:
+#         print("Los parametros estan mal")
+#         return False
 
-while True:
-    try:
-        print("""
-                    ---Menu---
-                1.-Comprar entrada
-                2.-Consultar comprador 
-                3.-Cancelar compra 
-                4.-Salir
-                """)
-        opc = int(input("Seleciona una opcion: "))
-        match opc:
-            case 1:
-                comprarEntrada(cliente)
-            case 2:
-                mostrarCliente(cliente)
-            case 3:
-                borrarCliente(cliente)
-            case 4:
-                print("Saliendo...")
-                break
-            case _:
-                print("Error, seleciona una opcion valida.")
-    except Exception as e:
-        print("Error.", e)
-                
+# while True:
+#     try:
+#         print("""
+#                     ---Menu---
+#                 1.-Comprar entrada
+#                 2.-Consultar comprador 
+#                 3.-Cancelar compra 
+#                 4.-Salir
+#                 """)
+#         opc = int(input("Seleciona una opcion: "))
+#         match opc:
+#             case 1:
+#                 comprarEntrada(cliente)
+#             case 2:
+#                 mostrarCliente(cliente)
+#             case 3:
+#                 borrarCliente(cliente)
+#             case 4:
+#                 print("Saliendo...")
+#                 break
+#             case _:
+#                 print("Error, seleciona una opcion valida.")
+#     except Exception as e:
+#         print("Error.", e)
+
+
+# ------------------------------------------------------------------------------------
+
+
